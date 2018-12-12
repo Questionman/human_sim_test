4761457f751ffc4b7e7975b4bd3fd97fde16d576
Questionman <diti94@naver.com>
2018-12-12 18:48:31 +0000

    the first commit

---
+++ build/CMakeFiles/human_phantom.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/human_phantom"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/human_phantom.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ build/CMakeFiles/phantom.dir/DependInfo.cmake
@@ -0,0 +1,103 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/phantom.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/phantom.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4BasePhantomBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4BasePhantomBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4CustomFemaleBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4CustomFemaleBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4FemaleBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4FemaleBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomActionInitialization.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomActionInitialization.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomAnalysisManager.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomAnalysisManager.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomColour.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomColour.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomConstruction.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomConstruction.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomEventAction.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomEventAction.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomHit.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomHit.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomMaterial.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomMaterial.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomMessenger.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomMessenger.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomPhysicsList.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomPhysicsList.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomPrimaryGeneratorAction.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomPrimaryGeneratorAction.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomRunAction.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomRunAction.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomSD.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomSD.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4HumanPhantomSteppingAction.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4HumanPhantomSteppingAction.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDBodyFactory.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDBodyFactory.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDBrain.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDBrain.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDHead.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDHead.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDHeart.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDHeart.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftAdrenal.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftAdrenal.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftArmBone.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftArmBone.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftBreast.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftBreast.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftClavicle.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftClavicle.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftKidney.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftKidney.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftLeg.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLeg.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftLegBone.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLegBone.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftLung.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftLung.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftOvary.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftOvary.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftScapula.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftScapula.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLeftTeste.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLeftTeste.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLiver.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLiver.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDLowerLargeIntestine.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDLowerLargeIntestine.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDMaleGenitalia.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDMaleGenitalia.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDMiddleLowerSpine.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDMiddleLowerSpine.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDPancreas.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDPancreas.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDPelvis.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDPelvis.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRibCage.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRibCage.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightAdrenal.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightAdrenal.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightArmBone.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightArmBone.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightBreast.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightBreast.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightClavicle.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightClavicle.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightKidney.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightKidney.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightLeg.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLeg.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightLegBone.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLegBone.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightLung.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightLung.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightOvary.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightOvary.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightScapula.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightScapula.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDRightTeste.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDRightTeste.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDSkull.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSkull.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDSmallIntestine.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSmallIntestine.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDSpleen.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDSpleen.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDStomach.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDStomach.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDThymus.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDThymus.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDThyroid.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDThyroid.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDTrunk.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDTrunk.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDUpperLargeIntestine.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUpperLargeIntestine.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDUpperSpine.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUpperSpine.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDUrinaryBladder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUrinaryBladder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MIRDUterus.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MIRDUterus.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4MaleBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4MaleBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4ORNLFemaleBodyFactory.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4ORNLFemaleBodyFactory.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4ORNLMaleBodyFactory.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4ORNLMaleBodyFactory.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4PhantomBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4PhantomBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4PhantomHeadBuilder.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4PhantomHeadBuilder.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4VBodyFactory.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4VBodyFactory.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4VOrgan.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4VOrgan.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4VoxelBreastFactory.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelBreastFactory.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4VoxelLeftBreast.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelLeftBreast.cc.o"
+  "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/src/G4VoxelRightBreast.cc" "/home/ho09diti/geant4-10.4.2/examples/advanced/human_phantom/build/CMakeFiles/phantom.dir/src/G4VoxelRightBreast.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/Geant4"
+  "/usr/local/include"
+  "../include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
